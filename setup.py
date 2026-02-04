c4e1d9a78eb4e0c15b541f90a2cf8f20a7468b2e
Joerg Franke <joerg.franke@pm.me>
2026-02-04 14:15:22 +0100

    update to v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="titan-oellm",
-    version="0.2.0",
+    version="0.2.1",
     description="OpenEuroLLM models and components for TorchTitan",
     packages=find_packages(),
     python_requires=">=3.10",
