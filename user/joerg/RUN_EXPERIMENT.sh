940bc2b206c0d407033a323109ba179768add673
joergfranke <joerg.franke@pm.me>
2026-02-18 11:49:06 +0100

    update run script

--- user/joerg/RUN_EXPERIMENT.sh
+++ user/joerg/RUN_EXPERIMENT.sh
@@ -1,12 +1,9 @@
 
 
 
-# run a local experiment
-TITAN_USER=joerg CONFIG=user/joerg/configs/debug.toml bash submit_job.sh --local
+# run on JUWELS interactive node (after srun --pty bash)
+TITAN_USER=joerg CLUSTER=juwels DATASET=slimpajama_627b TOKENIZER=neox CONFIG=qwen3_custom.tom bash submit_job.sh --local -- --model.flavor=125M --job.dump_folder=outputs/test_oellm --metrics.save_tb_folder=tb --optimizer.lr=0.0022 --parameter_logging.log-parameters --parameter_logging.log-gradients --training.local_batch_size=8
+
 
 # submit to JUWELS cluster (SLURM)
-TITAN_USER=joerg CLUSTER=juwels DATASET=slimpajama_627b TOKENIZER=neox CONFIG=user/joerg/configs/debug.toml TOKENIZER=neox bash submit_job.sh
-
-# run on JUWELS interactive node (after srun --pty bash)
-TITAN_USER=joerg CLUSTER=juwels DATASET=slimpajama_627b TOKENIZER=neox CONFIG=user/joerg/configs/debug.toml bash submit_job.sh --local --checkpoint.enable
-
+TITAN_USER=joerg DATASET=slimpajama_627b TOKENIZER=neox CLUSTER=juwels CONFIG=qwen3_custom.toml bash submit_job.sh --nodes=1 --time=0:15:00 --partition=develbooster -- --model.flavor=125M --job.dump_folder=outputs/test_oellm --metrics.save_tb_folder=tb --optimizer.lr=0.0022 --parameter_logging.log-parameters --parameter_logging.log-gradients --training.local_batch_size=8 --validation.enable --metrics.log_freq=500 --training.seq_len=2048 --training.steps=12000
