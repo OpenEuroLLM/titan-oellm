afeeb0e932842ac3002c465210ae41ab98fdc332
joergfranke <joerg.franke@pm.me>
2026-02-20 19:39:28 +0100

    add grid search experiment

---
+++ user/joerg/expt/test_grid.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Hyperparameter Grid: lr × nodes × budget × betas
+set -euo pipefail
+
+# Config
+export TITAN_USER=joerg
+CLUSTER=capella
+DATASET=nemotron_cc
+TOKENIZER=nemotron
+CONFIG=qwen3_custom.toml
+TIME=0:15:00
+
+# Fixed training params
+LBS=32
+GPUS=4
+SEQ=4096
+MODEL=125M
+
+# Grid
+LRS=(0.001)
+NODES=(2)
+BUDGETS_B=(5)  # in billions
+BETAS=("0.9,0.95")
+
+for lr in "${LRS[@]}"; do
+    for n in "${NODES[@]}"; do
+        for bb in "${BUDGETS_B[@]}"; do
+            for bp in "${BETAS[@]}"; do
+                b1=${bp%,*}; b2=${bp#*,}
+                budget=$((bb * 1000000000))
+                gbs=$((LBS * GPUS * n * SEQ))
+                steps=$((budget / gbs))
+                name="lr${lr}_n${n}_${bb}B_b${b1}${b2}"
+
+                ARGS="--model.flavor=$MODEL --job.experiment_folder=outputs/scale_token_budget_125M/$name \
+                --metrics.save_tb_folder=tb \
+                --optimizer.lr=$lr --optimizer.beta1=$b1 --optimizer.beta2=$b2 \
+                --training.local_batch_size=$LBS --training.seq_len=$SEQ --training.steps=$steps \
+                --validation.enable=true --validation.freq=1000 --checkpoint.enable=true --checkpoint.interval=5000 \
+                --parameter_logging.enabled --parameter_logging.log_interval=500 \
+                --parameter_logging.log_parameters --parameter_logging.log_gradients \
+                --parameter_logging.log_optimizer_states"
+
+                TITAN_USER=$TITAN_USER DATASET=$DATASET TOKENIZER=$TOKENIZER CLUSTER=$CLUSTER CONFIG=$CONFIG \
+                bash submit_job.sh --nodes=$n --time=$TIME -- $ARGS && sleep 1
+
+done; done; done; done
